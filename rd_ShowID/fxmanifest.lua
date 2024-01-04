2891d4ff120be57cb739a1ffa7f784c3f77b436d
RespectDevelopment <143709835+respectdevelopment@users.noreply.github.com>
2024-01-04 18:33:26 +0100

    Updated fxmanifest.lua
    Changed file: fxmanifest.lua

--- rd_ShowID/fxmanifest.lua
+++ rd_ShowID/fxmanifest.lua
@@ -1,15 +1,20 @@
+-- {{FX INFORMATION}} --
 fx_version 'cerulean'
 game 'gta5'
 
+
+-- {{Resource Information}} --
 author 'Respect Development'
 description 'Show ID script'
 version '1.0.0'
 
+
+-- {{Manifest}} --
 lua54 'yes'
 
 shared_scripts {
     'shared/config.lua',
-    --'@ox_lib/init.lua', -- UNMARK IT HERE, IF YOU WANT USE OXLIB NOTIFICATION
+    --'@ox_lib/init.lua', ⚠️Unmark, if you want use ox_lib notification⚠️
 }
 
 client_scripts {
